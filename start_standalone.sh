32a0b97c6a9ed72fa124b280df6bb5f17621491f
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-07-16 03:46:48 +0200

    Fix ports

--- start_standalone.sh
+++ start_standalone.sh
@@ -12,9 +12,9 @@ nohup python3 server.py \
   --listen \
   --api \
   --model ${MODEL} \
-  --listen-port 3001 \
-  --api-blocking-port 5001 \
-  --api-streaming-port 5006 &> /workspace/logs/textgen.log &
+  --listen-port 3000 \
+  --api-blocking-port 5000 \
+  --api-streaming-port 5005 &> /workspace/logs/textgen.log &
 
 echo "Starting RunPod Handler"
 export PYTHONUNBUFFERED=1
