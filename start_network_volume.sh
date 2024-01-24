fe7c771cd88027d16be2c70c5038457a47268452
rafa-9 <rafa.inspired9@gmail.com>
2024-01-24 03:48:20 -0500

    use transformer loader

--- start_network_volume.sh
+++ start_network_volume.sh
@@ -18,7 +18,7 @@ nohup python3 server.py \
   --listen \
   --api \
   --model ${MODEL} \
-  --loader exllama \
+  --loader transformer \
   --listen-port 3000 \
   --api-blocking-port 5000 \
   --api-streaming-port 5005 &> /workspace/logs/textgen.log &
